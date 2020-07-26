e4f2c058807297453980ba98b71f43be2c0610b7
TheZihanGu <i@zihangu.cn>
2020-07-26 23:04:49 +0800

    关于 - 页面内容更改

--- view/admin/about.php
+++ view/admin/about.php
@@ -3,22 +3,21 @@
 <div class="mdui-container-fluid">
 
 <div class="mdui-typo">
-	<h1> <a href="?/admin/about" target="_blank">关于</a>
+	<h1>关于</h1>
 </div>
 <div class="mdui-textfield">
-    <h3>OneIndex By TheZihanGu   <small>一个OneIndex改进版，保留原版的简洁。</small></h3>
+    <h3>OneIndex-TZG   <small>一个 OneIndex 分支改进版，保留原版的简洁。</small></h3>
     <h4>GitHub：<a href="https://github.com/TheZihanGu/oneindex-tzg">TheZihanGu/oneindex-tzg</a></h4>
-    <h4>目前项目正处于开发测试期，请留意Git Commit并有需要地对程序进行更新。</h4>
 </div>
 <div class="mdui-textfield">
-    <h2>重置OneIndex</h2>
-    <h4>此操作允许您重新配置OneIndex，重置后当前配置将被删除，请知晓。</h4>
+    <h2>重置 OneIndex</h2>
+    <h4>此操作允许您重新配置 OneIndex，重置后当前配置将被删除，请知晓。</h4>
 	<div class="mdui-container">
     <button class="mdui-btn mdui-btn-block mdui-color-theme-accent mdui-ripple" mdui-dialog="{target: '#reset'}">重置</button>
 
   <div class="mdui-dialog" id="reset">
     <div class="mdui-dialog-title">确定重置吗？</div>
-    <div class="mdui-dialog-content">你将永远丢失当前的配置！使用前请确保passthru函数可用。</div>
+    <div class="mdui-dialog-content">你将永远丢失当前的配置！使用前请确保 passthru 函数可用。</div>
     <div class="mdui-dialog-actions">
         <button class="mdui-btn mdui-ripple" mdui-dialog-close>算了吧</button>
         <a href="/view/admin/reset.php">
