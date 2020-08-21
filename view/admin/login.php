0da99b0ad00537b34204fc2ad0fca508c170b75a
TheZihanGu <i@zihangu.cn>
2020-08-21 22:36:29 +0800

    控制台登录 - 重新设计
    基于 Bootstrap Examples - Sign-in 修改开发。

--- view/admin/login.php
+++ view/admin/login.php
@@ -1,22 +1,45 @@
-<?php view::layout('install/layout')?>
-<?php view::begin('content');?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1.0, user-scalable=no"/>
+	<title>OneIndex <?php e($title);?></title>
+	<link rel="dns-prefetch" href="//cdn.bootcdn.net" />
+	<link rel="dns-prefetch" href="//oi.tzg6.app" />
+	<link rel="dns-prefetch" href="//g.tzg6.com" />
+	<link rel="dns-prefetch" href="//portal.azure.com" />
+	<link rel="dns-prefetch" href="//cdnjs.cloudflare.com" />
+	<script src="https://g.tzg6.com/common/jquery/3.5.1/jquery.min.js"></script>
+	<link rel="stylesheet" href="https://g.tzg6.com/common/bootstrap-4.5.2-dist/css/bootstrap.min.css">
+	<script src="https://g.tzg6.com/common/bootstrap-4.5.2-dist/js/bootstrap.min.js"></script>
 
-<!-- Begin page content -->
-<main role="main" class="flex-shrink-0">
-<br/>
-      <div class="container">
-	  <br><br><br>
-	  <h2>OneIndex 控制台 <small>请登录</small></h2>
-	  <form action="" method="post">
-		  <div class="form-group">
-		    <label>密码</label>
-		    <input name="password" class="form-control" type="password"/>
-		  </div>
-		  <br>
-		  <button class="btn btn-primary" type="submit">登录</button>
-	  </form>
-	</div>
-	
-</div>
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
 
-<?php view::end('content');?>
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="https://getbootstrap.com/docs/4.5/examples/sign-in/signin.css" rel="stylesheet">
+  </head>
+  <body class="text-center">
+  <form action="" method="post" class="form-signin">
+  <h1 class="h3 mb-3 font-weight-normal">OneIndex 控制台</h1>
+  <label for="inputPassword" class="sr-only">密码</label>
+  <input name="password" class="form-control" placeholder="密码" type="password"/>
+  <div class="checkbox mb-3">
+  </div>
+  <button class="btn btn-lg btn-primary btn-block" type="submit">登录</button>
+</form>
+</body>
+</html>
