a4820fc587d29764344966797d191aa719a772db
TheZihanGu <i@zihangu.cn>
2020-08-25 11:46:38 +0800

    关于 - 增加项目 GitHub Button

--- view/admin/about.php
+++ view/admin/about.php
@@ -2,11 +2,17 @@
 <?php view::begin('content');?>
 <div class="mdui-container-fluid">
 
+<!-- Place this tag in your head or just before your close body tag. -->
+<script async defer src="https://buttons.github.io/buttons.js"></script>
+
 <div class="mdui-typo">
 	<h1>关于</h1>
 </div>
 <div class="mdui-textfield">
     <h3>OneIndex-TZG   <small>一个 OneIndex 分支改进版，保留原版的简洁。</small></h3>
     <h4>GitHub：<a href="https://github.com/TheZihanGu/oneindex-tzg">TheZihanGu/oneindex-tzg</a></h4>
+    <a class="github-button" href="https://github.com/TheZihanGu/oneindex-tzg" data-icon="octicon-star" aria-label="Star TheZihanGu/oneindex-tzg on GitHub">Star</a>
+    <a class="github-button" href="https://github.com/TheZihanGu/oneindex-tzg/archive/master.zip" data-icon="octicon-download" aria-label="Download TheZihanGu/oneindex-tzg on GitHub">Download</a>
+    <a class="github-button" href="https://github.com/TheZihanGu/oneindex-tzg/issues" data-icon="octicon-issue-opened" aria-label="Issue TheZihanGu/oneindex-tzg on GitHub">Issue</a>
 </div>
 <?php view::end('content');?>
